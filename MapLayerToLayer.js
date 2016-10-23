04a4063463e7fdc92c5e8dd05fe30174869f8207
creativitRy <creativitRy@gmail.com>
2016-10-23 13:07:16 -0500

    Added warning for old version

--- MapLayerToLayer.js
+++ MapLayerToLayer.js
@@ -8,6 +8,9 @@
 
 ///////////CODE/////////////
 
+if (parseInt(org.pepsoft.worldpainter.Version.BUILD) <= 20160820173357)
+	throw "Update WorldPainter!";
+
 print('Script by ctRy');
 
 var fromLayer;
